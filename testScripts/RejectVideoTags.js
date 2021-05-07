a8a7fc40aa905ba25a51003dad1665d561fd4c58
Youliang Yu <youliang.yu@eluv.io>
2021-05-06 19:56:51 -0700

    Add hidden/rejected/added tags in video tags summary

--- testScripts/RejectVideoTags.js
+++ testScripts/RejectVideoTags.js
@@ -1,7 +1,8 @@
 const {ElvClient} = require("../src/ElvClient");
-const fs = require("fs");
 const path = require("path");
-var rejectedJSON = require('./rejected_tags.json');
+var fs = require("fs");
+var sizeof = require('object-sizeof')
+var rejectedJSON = require('./rejected_tags_merge_hidden.json');
 
 const ClientConfiguration = require("../TestConfiguration.json");
 
@@ -15,11 +16,27 @@ function AddTextField(t) {
     return {"text": t}
 }
 
+function arrayRemove(arr, value) {
+    return arr.filter(function(ele) {
+        return !value.includes(ele["text"]);
+    });
+}
+
+function arrayTrackRemove(arr, value) {
+    return arr.filter(function(ele) {
+        return !value.includes(ele["text"][0]);
+    });
+}
+
 const Reject = async (libId, objId, targetPath) => {
     const textFilePath = path.join(targetPath, "iqFailed.txt");
     try {fs.unlinkSync(textFilePath);} catch(error) {}
-    var fs = require('fs')
     var iqLogger = fs.createWriteStream(textFilePath, {flags: 'a'})
+
+    //// ++++++++++++ Track File +++++++++++++
+    //const tagFilePath = path.join(targetPath, "video-tags-tracks-0000.json");
+    //try {fs.unlinkSync(tagFilePath);} catch(error) {}
+    //var outputfile = fs.createWriteStream(tagFilePath, {flags: 'a'})
     try {
         const client = await ElvClient.FromConfigurationUrl({
             configUrl: ClientConfiguration["config-url"]
@@ -30,55 +47,91 @@ const Reject = async (libId, objId, targetPath) => {
         });
         await client.SetSigner({signer});
 
-        console.log("Adding rejected video tags " + libId + " " + objId);
+        console.log("Adding curated video tags " + libId + " " + objId);
         const videoTags = await client.ContentObjectMetadata({
             libraryId: libId,
             objectId: objId,
             metadataSubtree: "video_tags",
         });
         //console.log(videoTags)
-
-        if ("rejected_tags" in videoTags) {
-            console.log("Skip " + objId + ", video tag summarized");
-            return
+        var summary = videoTags.summary;
+        if ("hidden_tags" in summary.object_detection) {
+            console.log("Skip "+ libId +" "+ objId + " tags curated");
+            return;
         }
+        //// ++++++++++++ Track File +++++++++++++
+        //const fileData = await client.DownloadFile({
+        //    libraryId:  libId,
+        //    objectId: objId,
+        //    filePath: "/video_tags/video-tags-tracks-0000.json",
+        //    format: "arrayBuffer"
+        //});
+        //console.log(fileData);
+        //var enc = new TextDecoder();
+        //var videoTracks = JSON.parse(enc.decode(fileData));
+        //console.log(sizeof(videoTracks));
+        ////outputfile.write(videoTracks);
 
-        var celebrityTags = [];
-        var landmarkTags = [];
-        var logoTags = [];
-        var objectTags = [];
-        var ocrTags = [];
-        var segmentTags = [];
-        var sttTags = [];
         const map = {
-            "Object Detection": objectTags,
-            "Celebrity Detection": celebrityTags,
-            "Landmark Recognition": landmarkTags,
-            "Logo Detection": logoTags,
-            "Optical Character Recognition": ocrTags,
-            "Segment Labels": segmentTags,
-            "Speech to Text": sttTags
-        }
+            "Object Detection": [],
+            "Celebrity Detection": [],
+            "Landmark Recognition": [],
+            "Logo Detection": [],
+            "Optical Character Recognition": [],
+            "Segment Labels": [],
+            "Speech to Text": []
+        };
         const mapName = {
             "Object Detection": "object_detection",
             "Celebrity Detection": "celebrity_detection",
             "Landmark Recognition": "landmark_recognition",
             "Logo Detection": "logo_detection",
             "Optical Character Recognition": "optical_character_recognition",
-            "Segment Labels": "segment_labels",
+            "Segment Labels": "segment_levels",
             "Speech to Text": "speech_to_text"
-        }
+        };
 
-        var rejectedTags = {
-            "video_tags": {"rejected_tags": {}}
+        if ("segment_labels" in summary) {
+            summary["segment_levels"] = summary["segment_labels"];
+            delete summary["segment_labels"];
         }
+        var curatedTags = summary;
 
-        const rejected = rejectedJSON[objId]
+        if (objId in rejectedJSON) {
+            var rejected = rejectedJSON[objId];
+        } else {
+            var rejected = map;
+        }
+        var hidden = map;
+        hidden["Object Detection"] = ["Human face", "Person"];
         for (var feature in map) {
-            const tags = rejected[feature].map(AddTextField);
-            rejectedTags.video_tags.rejected_tags[mapName[feature]] = {"tags": tags}
+            curatedTags[mapName[feature]] = {
+                "added_tags": [],
+                "hidden_tags": hidden[feature].map(AddTextField),
+                "curated_tags": arrayRemove(arrayRemove(summary[mapName[feature]]["tags"], rejected[feature]), hidden[feature]),
+                "rejected_tags": rejected[feature].map(AddTextField),
+                "tags": summary[mapName[feature]]["tags"]
+            }
         }
-        console.log(JSON.stringify(rejectedTags))
+        //console.log(JSON.stringify(curatedTags))
+
+        //// ++++++++++++ Track File +++++++++++++
+        //mapName["Segment Labels"] = "segment_levels"
+        //var curatedVideoTracks = videoTracks;
+        //for (var feature in map) {
+        //    console.log(mapName[feature])
+        //    curatedVideoTracks.metadata_tags[mapName[feature]]["rejected_tags"] = rejected[feature].map(AddTextField);
+        //    curatedVideoTracks.metadata_tags[mapName[feature]]["curated_tags"] = arrayTrackRemove(videoTracks.metadata_tags[mapName[feature]]["tags"], rejected[feature])
+        //}
+        //shotTags = videoTracks.metadata_tags.shot_tags["tags"]
+        //curatedVideoTracks.metadata_tags.shot_tags["curated_tags"] = shotTags
+        //for (i=0; i<shotTags.length; i++) {
+        //    for (var feature in map) {
+        //        curatedVideoTracks.metadata_tags.shot_tags["curated_tags"][i].text[feature] = arrayTrackRemove(shotTags[i].text[feature], rejected[feature])
+        //    }
+        //}
+        //console.log(curatedVideoTracks)
+        //console.log(sizeof(curatedVideoTracks));
 
         console.log("Update content object with rejected tags...")
         const edit = await client.EditContentObject({
@@ -86,34 +139,49 @@ const Reject = async (libId, objId, targetPath) => {
             objectId: objId,
         })
         console.log(edit)
-        if ("rejected_tags" in videoTags) {
-            await client.ReplaceMetadata({
-                libraryId: libId,
-                objectId: objId,
-                writeToken: edit["write_token"],
-                metadataSubtree: "video_tags/rejected_tags",
-                metadata: rejectedTags["video_tags"]["rejected_tags"]
-            })
-        } else {
-            await client.MergeMetadata({
-                libraryId: libId,
-                objectId: objId,
-                writeToken: edit["write_token"],
-                metadata: rejectedTags
-            })
-        }
+
+        //// ++++++++++++ Track File +++++++++++++
+        //await client.UploadFiles({
+        //    libraryId: libId,
+        //    objectId: objId,
+        //    writeToken: edit["write_token"],
+        //    fileInfo: [
+        //        {
+        //          "path": "/video_tags/video-tags-tracks-0000.json",
+        //          "type": "file",
+        //          "mime_type": "text/json",
+        //          "size": sizeof(curatedVideoTracks),
+        //          "data": curatedVideoTracks
+        //        }
+        //    ],
+        //})
+
+        await client.DeleteMetadata({
+            libraryId: libId,
+            objectId: objId,
+            writeToken: edit["write_token"],
+            metadataSubtree: "/video_tags/rejected_tags"
+        });
+
+        await client.ReplaceMetadata({
+            libraryId: libId,
+            objectId: objId,
+            writeToken: edit["write_token"],
+            metadataSubtree: "video_tags/summary",
+            metadata: curatedTags
+        })
         console.log("Finalize content object with summary tags...")
         const res = await client.FinalizeContentObject({
             libraryId: libId,
             objectId: objId,
             writeToken: edit["write_token"],
-            commitMessage: "Add rejected video tags"
+            commitMessage: "Add hidden video tags"
         })
         console.log(res);
         console.log("Finished " + libId + " " + objId);
 
     } catch(error) {
-        iqLogger.write(objId + '\n')
+        iqLogger.write(objId + '\n');
         console.error("Unrecoverable error:");
         console.log(JSON.stringify(error, null, 2));
         console.error(error.body ? error.body : error);
